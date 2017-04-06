391531feb97d3aca500885da523299a4fa48308b
Yuxfei <1069708350@qq.com>
2017-04-06 23:07:28 +0800

    bug fix

--- Digital-Image-Watermark/001_Direct_Text_Watermark/main.cpp
+++ Digital-Image-Watermark/001_Direct_Text_Watermark/main.cpp
@@ -10,15 +10,16 @@ int main()
 	Mat img = imread("./image/wallhaven-21738.jpg");
 	imshow("原始图像", img);
 
-	string text = "Yuxfei!";
-	double textSize = 3.0;
-	int colorNum = 128;
-	int textWidth = 3;
+	string text = "Yuxfei!"; // 水印文字
+	double textSize = 3.0; // 文字大小
+	int colorNum = 128; // 文字颜色
+	int textWidth = 3; // 文字宽度
 
 	putText(img, text, cv::Point(img.cols * 0.1111, img.rows * 0.8888),
 		cv::FONT_HERSHEY_PLAIN, textSize, cv::Scalar(colorNum, colorNum, colorNum), textWidth);
 
 	imshow("水印图像", img);
+	imwrite("水印图像.jpg", img);
 
 	waitKey(0);
 	return 0;
